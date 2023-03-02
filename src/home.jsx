55af682a4ad30c49f2f8cee5c50e6a8993502755
Juan David Guzman A <juan_dav.guzman@uao.edu.co>
2023-03-01 22:49:06 -0500

    Challenge2

--- src/home.jsx
+++ src/home.jsx
@@ -1,24 +1,15 @@
-import { useState } from 'react';
+
 import './App.css'
-import { FirstApp, Counter, GifExpert, GifGrid, Counter2, Fetch_component, BreakingBad } from './'
+import { Counter } from './Challenge2'
 
 const Home = () =>{
 
     return (
         <>
-            <FirstApp />
-            <hr />
+
             <Counter />
             <hr />
-            <GifExpert/>
-            <hr />
-            <GifGrid/>
-            <hr />
-            <Counter2/>
-            <hr />
-            <Fetch_component/>
-            <hr />
-            <BreakingBad/>
+
         </>
       )
 }
